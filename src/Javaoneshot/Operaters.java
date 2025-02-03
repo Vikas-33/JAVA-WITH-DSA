b00ec15f9c4c37569ef4fba7a9d874483a4c1652
Vikas Kumar Kumawat <vikasku.kal@gmail.com>
2025-02-03 18:15:34 +0530

    Added a Java class demonstrating basic operators.

---
+++ src/Javaoneshot/Operaters.java
@@ -0,0 +1,27 @@
+package Javaoneshot;
+
+public class Operaters {
+    public static void main(String[] args) {
+        //ops
+        int a=7;
+        int b=3;
+        int add=a+b;
+        int sub=a-b;
+        int mul=a*b;
+        double div=a/b;
+        System.out.println("addition = "+add);
+        System.out.println("subtraction = "+sub);
+        System.out.println("multiplication = "+mul);
+        System.out.println("division = "+div);
+        double modulo=a%b;
+        System.out.println("modulo ="+modulo);
+
+        //unary ops
+        a++;
+        System.out.println("increased a = "+a);
+        a--;
+        System.out.println("decrised a ="+a);
+
+
+    }
+}
