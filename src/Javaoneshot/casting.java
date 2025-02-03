4135c26b8546e92ff5bc6295371fbfc537c997ed
Vikas Kumar Kumawat <vikasku.kal@gmail.com>
2025-02-03 18:02:11 +0530

    Added a Java class demonstrating basic casting operations.

---
+++ src/Javaoneshot/casting.java
@@ -0,0 +1,28 @@
+package Javaoneshot;
+
+public class casting {
+    public static void main(String[] args) {
+        double price=100;
+        double finalPrice=price+18;// double has more capacity then int
+        System.out.println(finalPrice);
+
+//        int p=100;
+//        int fp=p+18.0;    //it will give error bcs int has low capacity then double
+//        System.out.println(fp);
+
+        //explicit casting
+        int p=100;
+        int fp=p+(int)18.3;   // casted double value 18.3 int int value 18
+        System.out.println(fp);
+
+
+        //constants
+        int age=30;
+        age=31;
+        age=32;
+
+        final float PI=3.14F;
+
+
+    }
+}
