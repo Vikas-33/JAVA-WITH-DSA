7f236b25a85e36c9111e70da0478938ed75a7d8b
Vikas Kumar Kumawat <vikasku.kal@gmail.com>
2025-02-28 03:45:08 +0530

    .

--- src/JAVA/Loops2.java
+++ src/JAVA/Loops2.java
@@ -5,22 +5,21 @@ import java.util.Scanner;
 public class Loops2 {
     public static void main(String[] args) {
 
-//        count digits in number
+//       count digits in number
         Scanner sc =new Scanner(System.in);
-//        System.out.print("Enter the number: ");
-//        int n=sc.nextInt();
-//        int count=0;
-//        if (n==0){
-//            System.out.println("The digit count in number is: "+count);
-//        }
-//        else {
-//            while (n > 0) {
-//                n /= 10;
-//                count++;
-//
-//            }
-//        }
-//        System.out.println("The count of digits is: "+count);
+        System.out.print("Enter the number: ");
+        int n=sc.nextInt();
+        int count=0;
+        if (n==0){
+            System.out.println("The digit count in number is: "+count);
+        }
+        else {
+            while (n > 0) {
+                n /= 10;
+                count++;
+            }
+        }
+        System.out.println("The count of digits is: "+count);
 
 
 
@@ -29,15 +28,15 @@ public class Loops2 {
 
 
 //        sum of digits of a number
-//        Scanner sc =new Scanner(System.in);
-//        System.out.print("Enter the number: ");
-//        int n=sc.nextInt();
-//        int sum=0;
-//        while (n>0){
-//            sum+=n%10;
-//            n/=10;
-//        }
-//        System.out.println("The sum of digits is: "+sum);
+        Scanner sc =new Scanner(System.in);
+        System.out.print("Enter the number: ");
+        int n=sc.nextInt();
+        int sum=0;
+        while (n>0){
+            sum+=n%10;
+            n/=10;
+        }
+        System.out.println("The sum of digits is: "+sum);
 
 
 
@@ -47,14 +46,14 @@ public class Loops2 {
 
 
 //        reverse a number
-//        System.out.print("Enter the number: ");
-//        int n= sc.nextInt();
-//        int reversenumber;
-//        while (n>0){
-//            reversenumber=n%10;
-//            n/=10;
-//            System.out.print(reversenumber);
-//        }
+        System.out.print("Enter the number: ");
+        int n= sc.nextInt();
+        int reversenumber;
+        while (n>0){
+            reversenumber=n%10;
+            n/=10;
+            System.out.print(reversenumber);
+        }
 
 
 
