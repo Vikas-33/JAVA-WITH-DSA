fff0f15c8f0f1572e203dfc03cf703ac54474c0c
Vikas Kumar Kumawat <vikasku.kal@gmail.com>
2025-02-27 13:20:50 +0530

    added some loop problems.

---
+++ src/JAVA/Loops2.java
@@ -0,0 +1,68 @@
+package JAVA;
+
+import java.util.Scanner;
+
+public class Loops2 {
+    public static void main(String[] args) {
+
+//        count digits in number
+        Scanner sc =new Scanner(System.in);
+//        System.out.print("Enter the number: ");
+//        int n=sc.nextInt();
+//        int count=0;
+//        if (n==0){
+//            System.out.println("The digit count in number is: "+count);
+//        }
+//        else {
+//            while (n > 0) {
+//                n /= 10;
+//                count++;
+//
+//            }
+//        }
+//        System.out.println("The count of digits is: "+count);
+
+
+
+
+
+
+
+//        sum of digits of a number
+//        Scanner sc =new Scanner(System.in);
+//        System.out.print("Enter the number: ");
+//        int n=sc.nextInt();
+//        int sum=0;
+//        while (n>0){
+//            sum+=n%10;
+//            n/=10;
+//        }
+//        System.out.println("The sum of digits is: "+sum);
+
+
+
+
+
+
+
+
+//        reverse a number
+//        System.out.print("Enter the number: ");
+//        int n= sc.nextInt();
+//        int reversenumber;
+//        while (n>0){
+//            reversenumber=n%10;
+//            n/=10;
+//            System.out.print(reversenumber);
+//        }
+
+
+
+
+
+
+
+
+
+    }
+}
